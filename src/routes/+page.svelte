820616bfd1d0b0f3525bed166d49b6c4dc53cff2
portjm <jose.porta@ucf.edu>
2024-12-22 21:16:05 -0500

    updated main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,43 @@
 <svelte:head>
     <title>J.M. Porta</title>
 </svelte:head>
-<main class=" flex-grow grid p-4 bg-zinc-900 place-content-center w-screen">
-    <div id="main-card" class=" h-72 w-96 p-10">
-        <div id="main-card-contents">
-            <h1 class="text-center">Testing Testing</h1>
+<main class=" flex-grow flex p-4 bg-zinc-900 place-content-center w-screen">
+    <div id="main-card" class=" flex-grow content-center px-16 pb-36">
+        <div id="main-card-contents" class="text-center">
+            <h1 class="text-5xl text-zinc-50">Testing Testing</h1>
             <p>
                 I know absolutely nothing about CI/CD so I hope this works. 🤞
             </p>
+            <div class=" mt-7">
+                <h2 class=" text-2xl text-zinc-300">LINKS</h2>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Magni iusto debitis, ullam dolores sapiente odit tempore
+                    maxime similique fuga aliquid recusandae ex harum! Ex libero
+                    dolorum assumenda officiis eveniet quae! Lorem ipsum dolor
+                    sit amet consectetur adipisicing elit. Id illum autem amet
+                    aliquam voluptatum quasi culpa, quos iste quod vel sit
+                    doloribus repellat illo maiores, ut provident cupiditate
+                    tempora! Earum. Lorem ipsum, dolor sit amet consectetur
+                    adipisicing elit. Asperiores laborum ex ipsum, dolores ea
+                    commodi optio sed nesciunt in dolorum et reiciendis odit
+                    ducimus voluptatem sint debitis fugit explicabo minima.
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Molestiae similique voluptatum, reprehenderit dignissimos
+                    numquam delectus dolore nisi alias sit aperiam atque magni
+                    ea, praesentium nulla ipsam ad velit accusantium! Et?
+                </p>
+
+                <p></p>
+            </div>
         </div>
     </div>
 </main>
 
 <style>
-    h1 {
-        @apply text-3xl;
-    }
     #main-card {
         position: relative;
         z-index: 2;
-        background-color: whitesmoke;
-    }
-
-    #main-card-contents {
-        position: relative;
-        z-index: 1;
+        background-color: rgba(240, 240, 240, 0.4);
     }
 </style>
