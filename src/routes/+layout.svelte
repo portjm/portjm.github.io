ec1aae4a7bcaa515a1c18eff943268e7f3537c54
portjm <jose.porta@ucf.edu>
2024-12-22 21:04:16 -0500

    updated CI/CD stuff

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import "../app.css";
+
+    let { children } = $props();
+</script>
+
+<div class="flex flex-col min-h-screen">
+    <header
+        class=" h-20 bg-zinc-900 content-center border-solid border-b-2 border-slate-300 font-serif"
+    >
+        <h1 class=" text-5xl text-slate-50 pl-9">Portfolio</h1>
+    </header>
+    {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<svelte:head>
+    <title>J.M. Porta</title>
+</svelte:head>
+<main class=" flex-grow grid p-4 bg-zinc-900 place-content-center w-screen">
+    <div id="main-card" class=" h-72 w-96 p-10">
+        <div id="main-card-contents">
+            <h1 class="text-center">Welcome to SvelteKit</h1>
+            <p>
+                Visit <a href="https://svelte.dev/docs/kit"
+                    >svelte.dev/docs/kit</a
+                > to read the documentation
+            </p>
+        </div>
+    </div>
+</main>
+
+<style>
+    h1 {
+        @apply text-3xl;
+    }
+    #main-card {
+        position: relative;
+        z-index: 2;
+        background-color: whitesmoke;
+    }
+
+    #main-card-contents {
+        position: relative;
+        z-index: 1;
+    }
+</style>
