b4c45d3f7e16854e56708f2ff4c13d4b36ff63dd
portjm <jose.porta@ucf.edu>
2024-12-24 13:11:55 -0500

    slightly redesigned theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,13 +4,26 @@
     let { children } = $props();
 </script>
 
-<div class="flex flex-col min-h-screen bg-zinc-900">
+<div class=" grid-background flex flex-col min-h-screen bg-zinc-900">
     <header
         class=" h-20 bg-zinc-950 content-center border-solid border-b-2 border-[#D10000] font-serif"
     >
         <h1 class=" text-5xl text-slate-200 sm:pl-9 sm:text-left text-center">
-            Portfolio
+            J.M. P&#9676;rta
         </h1>
     </header>
     {@render children()}
 </div>
+
+<style>
+    .grid-background {
+        background-size: 40px 40px;
+        position: relative;
+        background-image: linear-gradient(
+                to right,
+                rgb(75, 75, 75) 1px,
+                transparent 1px
+            ),
+            linear-gradient(to bottom, rgb(75, 75, 75) 1px, transparent 1px);
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,16 @@
 <svelte:head>
-    <title>J.M. Porta</title>
+    <title>Portfolio [WIP]</title>
 </svelte:head>
-<main
-    class=" flex-grow grid lg:grid-rows-3 gap-4 p-0 place-content-center xl:m-auto w-screen"
->
-    <div
-        id="main-card"
-        class=" lg:row-span-2 flex-grow content-center lg:px-5 py-10 border-solid border-b-2 border-b-white"
-    >
-        <div
-            id="main-card-contents"
-            class="lg:w-3/5 lg:m-auto text-center py-18 xl:px-48 px-6"
-        >
-            <h1 class="lg:text-8xl md:text-5xl text-4xl text-zinc-50 mb-4">
-                Hi, I'm J.M.
+<main class=" flex-grow">
+    <div class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit">
+        <div id="main-card" class="  lg:mt-10 mt-4 mx-auto p-8 text-center">
+            <h1 class="lg:text-6xl md:text-5xl text-4xl text-zinc-50 mb-0">
+                J&#8855;se M. P&#8855;rta
             </h1>
-            <p id="main-subhead" class="text-xl text-[#D10000]">
+            <p id="main-subhead" class="text-xl text-[#D10000] mb-6">
                 Software Developer &#9663; AI Researcher
             </p>
-            <br />
-            <p class="text-white">
+            <p>
                 I know absolutely nothing about CI/CD so I hope this works. 🤞<br
                 />
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni iusto
@@ -38,54 +29,52 @@
             </p>
         </div>
     </div>
-    <div class=" bg-stone-500 px-16 py-10 lg:w-3/5 lg:mx-auto xl:px-24 px-6">
-        <h2 class=" text-3xl text-zinc-300 text-center mb-3">PROJECTS</h2>
-        <p class="text-white">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio
-            expedita voluptate soluta esse consequuntur adipisci et quaerat ipsa
-            libero, omnis cumque. Veniam mollitia dignissimos debitis voluptatum
-            inventore quam veritatis nam. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Magni iusto debitis, ullam dolores sapiente odit
-            tempore maxime similique fuga aliquid recusandae ex harum! Ex libero
-            dolorum assumenda officiis eveniet quae! Lorem ipsum dolor sit amet
-            consectetur adipisicing elit. Id illum autem amet aliquam voluptatum
-            quasi culpa, quos iste quod vel sit doloribus repellat illo maiores,
-            ut provident cupiditate tempora! Earum. Lorem ipsum, dolor sit amet
-            consectetur adipisicing elit. Asperiores laborum ex ipsum, dolores
-            ea commodi optio sed nesciunt in dolorum et reiciendis odit ducimus
-            voluptatem sint debitis fugit explicabo minima. Lorem ipsum dolor
-            sit amet, consectetur adipisicing elit. Molestiae similique
-            voluptatum, reprehenderit dignissimos numquam delectus dolore nisi
-            alias sit aperiam atque magni ea, praesentium nulla ipsam ad velit
-            accusantium! Et?<br />
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati,
-            ipsa molestiae iste eum eius ab ut expedita, nisi similique sapiente
-            animi deleniti consequuntur amet voluptatibus hic impedit? Repellendus,
-            laudantium non! Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Voluptates cumque laboriosam provident molestiae tenetur nisi aperiam
-            illum. Dicta nulla quos quas asperiores porro! Impedit odit eius soluta
-            adipisci ea quos? Lorem ipsum dolor sit amet, consectetur adipisicing
-            elit. Pariatur hic eum asperiores aliquid deserunt nemo tempore. Nesciunt
-            sapiente labore quisquam dolorem in, illo vero. Deleniti praesentium
-            quasi error reprehenderit eveniet. Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Corrupti sed ad odit laudantium. Perspiciatis non modi
-            dolore voluptatum odio aut, est nostrum assumenda itaque dolorem cum
-            voluptatem cumque consectetur eos. Lorem ipsum dolor, sit amet consectetur
-            adipisicing elit. Repellendus modi at minus similique cupiditate voluptatum
-            iusto dolor velit nisi quia. Cumque perspiciatis quisquam dolor aperiam
-            incidunt saepe nulla officiis est! Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Autem maxime molestiae tempora a qui ad quasi recusandae.
-            Alias, ullam dolorum. Aspernatur, nihil blanditiis! Consequuntur praesentium
-            reiciendis provident inventore ullam dolorum!
-        </p>
+
+    <div
+        class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit mt-10"
+    >
+        <div class="p-8">
+            <h1 class=" text-4xl mb-6">Technical Specs</h1>
+            <div id="tech-logos">
+                <img src="/rs.png" width="100px" height="100px" alt="Rust" />
+                <img src="/py.png" width="100px" alt="Python" />
+                <img src="/pytor.png" width="200px" alt="PyTorch" />
+                <img src="/sv.png" width="100px" alt="Svelte/Sveltekit" />
+            </div>
+        </div>
     </div>
 </main>
 
 <style>
     #main-card {
-        background-image: url("/bg.jpg");
-        background-size: cover;
-        background-position: center;
-        box-shadow: inset 0 0 0 2000px rgba(9, 9, 11, 0.9);
+        width: 100%;
+        height: 100%;
+    }
+
+    .container {
+        position: relative;
+        z-index: 1;
+    }
+
+    .container::before {
+        content: "";
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        top: 0;
+        left: 0;
+        z-index: -1;
+    }
+
+    .container:hover::before {
+        border: 1px solid #d10000;
+    }
+    #tech-logos {
+        display: flex;
+        justify-content: space-around;
+        flex-wrap: wrap;
+    }
+    #tech-logos img {
+        align-self: center;
     }
 </style>
