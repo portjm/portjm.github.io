1816fb6666ad33aefb6ab9d18d789a70b751a3ad
portjm <jose.porta@ucf.edu>
2024-12-26 19:22:24 -0500

    Added projects section

---
+++ src/lib/components/PCard.svelte
@@ -0,0 +1,13 @@
+<script>
+    let { name = "Project 0", description } = $props();
+</script>
+
+<div class=" bg-slate-300 p-4 pb-6">
+    <h3 class=" text-2xl text-zinc-800 font-semibold">{name}</h3>
+    <p class="text-zinc-500 text-sm">subheader</p>
+    {#if description}
+        <p class="mt-4 text-zinc-800">
+            {@render description()}
+        </p>
+    {/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+    import PCard from "$lib/components/PCard.svelte";
+</script>
+
 <svelte:head>
     <title>Portfolio [WIP]</title>
 </svelte:head>
@@ -5,7 +9,7 @@
     <div class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit">
         <div id="main-card" class="  lg:mt-10 mt-4 mx-auto p-8 text-center">
             <h1 class="lg:text-6xl md:text-5xl text-4xl text-zinc-50 mb-0">
-                J&#8855;se M. P&#8855;rta
+                Jose M. Porta
             </h1>
             <p id="main-subhead" class="text-xl text-[#D10000] mb-6">
                 Software Developer &#9663; AI Researcher
@@ -31,15 +35,49 @@
     </div>
 
     <div
-        class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit mt-10"
+        class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit mt-8"
     >
-        <div class="p-8">
+        <div class="md:p-6 p-3">
             <h1 class=" text-4xl mb-6">Technical Specs</h1>
-            <div id="tech-logos">
+            <div id="tech-logos" class="md:mx-0 mx-3">
                 <img src="rs.png" width="100px" height="100px" alt="Rust" />
                 <img src="py.png" width="100px" alt="Python" />
                 <img src="pytor.png" width="200px" alt="PyTorch" />
                 <img src="sv.png" width="100px" alt="Svelte/Sveltekit" />
+            </div>
+        </div>
+    </div>
+
+    <div
+        class="container text-zinc-200 bg-zinc-800/85 2xl:w-2/5 mx-auto h-fit mt-8"
+    >
+        <div class="md:p-6 p-3">
+            <h1 class=" text-4xl mb-6">Projects</h1>
+            <div class="grid lg:grid-cols-2 gap-4 gap-y-8 items-start w-full">
+                <PCard name="Project 0">
+                    {#snippet description()}
+                        <img
+                            src="https://placehold.co/600x300"
+                            alt="placeholder"
+                        />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Minus labore a reprehenderit. Enim earum nam, quam tenetur
+                        itaque doloribus reprehenderit voluptate hic expedita at
+                        fugit dolores adipisci blanditiis, mollitia qui!
+                    {/snippet}
+                </PCard>
+                <PCard name="Project 1">
+                    {#snippet description()}
+                        <img
+                            src="https://placehold.co/600x300"
+                            alt="placeholder"
+                        />
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Exercitationem dicta officia aliquam sit magni dignissimos
+                        labore placeat numquam id iste fugiat molestiae natus architecto
+                        dolore, impedit, iusto saepe tempora ipsum!
+                    {/snippet}
+                </PCard>
             </div>
         </div>
     </div>
@@ -68,10 +106,11 @@
 
     .container:hover::before {
         border: 1px solid #d10000;
+        box-shadow: 0px 0px 10px #d1000077;
     }
     #tech-logos {
         display: flex;
-        justify-content: space-around;
+        justify-content: space-between;
         flex-wrap: wrap;
     }
     #tech-logos img {
